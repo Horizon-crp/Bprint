56e61365319c06988cfb5a857250d8f923471098
Noémie <102274923+Name-shitty-github-profile@users.noreply.github.com>
2022-09-21 21:03:49 +0200

    Create aprint.py

---
+++ src/aprint.py
@@ -0,0 +1,21 @@
+from threading import Thread
+from sys import stdout
+from typing import Optional
+async def aprint(*values, sep: Optional[str] = ' ', end: Optional[str] = '\n', File = stdout, flush: bool = False):
+  '''
+  The async version of tprint (works with threading too)
+  '''
+  sep = str(sep)
+  e: str = ''
+  for i in values:
+    e += str(i) + sep
+  e += end
+  if flush is False:
+    def write():
+      File.write(e)
+  else:
+    def write():
+      File.write(e)
+      flush()
+  Thread(target=write).start()
+  return None
